fd5c1c99e97be49740a1c191d8fc418302d2e845
Scholz <mail@stefen-scholz.de>
2024-10-04 14:58:11 +0200

    Change app identifier for playstore to de.hippocal.app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'de.hippocal.mobile',
+  appId: 'de.hippocal.app',
   appName: 'HippoCal',
   webDir: 'www',
   server: {
