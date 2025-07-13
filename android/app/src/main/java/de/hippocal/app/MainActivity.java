567f8a2ab01b2b06a181dbd6bfd7470e139c2008
Steffen <mail@steffen-scholz.de>
2025-07-13 15:06:48 +0200

    Verbesserte Android-16 Kompatibilität

--- android/app/src/main/java/de/hippocal/app/MainActivity.java
+++ android/app/src/main/java/de/hippocal/app/MainActivity.java
@@ -1,5 +1,17 @@
 package de.hippocal.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        // Enable WebView debugging for USB debugging
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+    }
+}
